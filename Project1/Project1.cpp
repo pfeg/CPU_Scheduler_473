d97e4e8ea5cd820eca55235cd3f8cba05a6385f5
Thomas Parker Phegley <phegleytp@gmail.com>
2017-02-24 18:57:37 -0500

    (feat)Process thread arrival.

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -2,9 +2,45 @@
 //
 
 #include "myscheduler.h"
+#include <list>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void PrintThreads(list<ThreadsList*> threadList);
 
 int main(int argc, char* argv[])
 {
+	MyScheduler scheduler = MyScheduler(FCFS, 1);
+
+	srand(time(NULL));
+
+	for (int i = 0; i < 10; i++)
+	{
+		scheduler.CreateThread(rand() % 100, 100, 1, 7);
+	}
+
+	PrintThreads(scheduler.Threads);
+
+	scheduler.Go();
+
+	PrintThreads(scheduler.ReadyQueue);
+
 	return 0;
+}
+
+void PrintThreads(list<ThreadsList*> threadList)
+{
+	list<ThreadsList*>::iterator iter;
+	iter = threadList.begin();
+
+	while (iter != threadList.end())
+	{
+		cout << "[" << (*iter)->ThreadInfo.tid << "|" << (*iter)->ThreadInfo.priority << "|" << (*iter)->ThreadInfo.arriving_time << "|" << (*iter)->ThreadInfo.remaining_time << "]->" << endl;
+		iter++;
+	}
+
+	cout << endl << endl;
 }
 
