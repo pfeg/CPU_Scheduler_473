65134957ae0d2e10fe6edf5d23a117bdbb03a13a
Thomas Parker Phegley <phegleytp@gmail.com>
2017-02-24 17:56:17 -0500

    Initial commit

---
+++ Project1/myscheduler.cpp
@@ -0,0 +1,41 @@
+//myschedule.cpp
+/*Define all the functions in 'myschedule.h' here.*/
+#include "myscheduler.h"
+
+void MyScheduler::CreateThread(int arriving_time, int remaining_time, int priority, int tid) //Thread ID not Process ID
+{
+	//Function to Create Thread(s) and insert them in the student
+	//defined data structure
+	ThreadsList *temp;
+	temp->ThreadInfo.tid = tid;
+	temp->ThreadInfo.arriving_time = arriving_time;
+	temp->ThreadInfo.remaining_time = remaining_time;
+	temp->ThreadInfo.priority = priority = priority;
+
+	QueuedThreads.push_back(temp);
+}
+
+bool MyScheduler::Dispatch()
+{
+	//Todo: Check and remove finished threads
+	//Todo: Check if all the threads are finished; if so, return false
+	switch(policy)
+	{
+		case FCFS:		//First Come First Serve
+
+			break;
+		case STRFwoP:	//Shortest Time Remaining First, without preemption
+
+			break;
+		case STRFwP:	//Shortest Time Remaining First, with preemption
+
+			break;
+		case PBS:		//Priority Based Scheduling, with preemption
+
+			break;
+		default :
+			cout<<"Invalid policy!";
+			throw 0;
+	}
+	return true;
+}
---
+++ Project1/myscheduler.h
@@ -0,0 +1,49 @@
+#pragma once
+//myschedule.h
+/* Students need to define their own data structure to contain
+   and access objects from 'thread class'. The 'CreateThread()' 
+   function should insert the threads into the student defined
+   data structure after creating them.
+   They are allowed to add any additional functionality (only 
+   declaration in this file, define in 'myschedule.cpp')
+   which they might find helpful.*/
+
+#include "scheduler.h"
+#include <list>
+
+//Define your data structure here.
+	/*
+	*
+	*
+	*
+	*
+	*/
+class ThreadsList {
+public:
+
+	ThreadDescriptorBlock ThreadInfo;
+
+private:
+
+};
+
+class MyScheduler: public Scheduler {
+public:
+	MyScheduler(Policy p, unsigned int n) : Scheduler(p, n) {}
+	bool Dispatch() override; //Function to implement scheduling policy and to keep a check on processed threads
+	void CreateThread(int arriving_time, int remaining_time, int priority, int tid) override; //Function to create threads and insert them in student defined data structure
+
+	// a list of Threads ready to be executed.
+	list<ThreadsList*> ReadyQueue;
+
+	// a list of all the Threads;
+	list<ThreadsList*> Threads;
+
+	//Declare additional methods(s) below if needed.
+	/*
+	*
+	*
+	*
+	*
+	*/
+};
